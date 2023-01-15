65b4b63d11e52f0239dfd77c13913471edea2d27
Quinn Le <quynhnle135@gmail.com>
2023-01-15 09:30:13 -0500

    Learned about Class and Instance in a class

---
+++ object_oriented_programming/player.py
@@ -0,0 +1,26 @@
+class Player:
+    teamName = "Liverpool" # class variables
+    teamMembers = []
+
+    def __init__(self, name):
+        self.name = name # instance variables
+        self.formerTeams = []
+        self.teamMembers.append(self.name)
+
+
+p1 = Player('Mark')
+p1.formerTeams.append('Barcelona')
+print("Name: ", p1.name)
+print("Team name: ", p1.teamName)
+print("Team member: ", p1.teamMembers)
+
+
+p2 = Player('Steve')
+p2.formerTeams.append('Chelsea')
+p2.formerTeams.append('Manchester')
+
+# p2.team_name = "Manchester"
+print(f"Name: {p2.name}")
+print(f"Team name: {p2.teamName}")
+print(p2.formerTeams)
+print("Team members: ", p2.teamMembers)
