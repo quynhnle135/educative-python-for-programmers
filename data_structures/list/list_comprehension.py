e33e0cb6eabf44f87531e1ab84930f9620f4180e
Quinn Le <quynhnle135@gmail.com>
2023-03-09 19:01:45 -0500

    Learn more about list operations

--- data_structures/list/list_comprehension.py
+++ data_structures/list/list_comprehension.py
@@ -22,3 +22,27 @@ print(sum_list)
 new_list = list1 + list2
 new_list.sort()
 print(new_list)
+
+num_seq = range(0, 11)
+num_list = list(num_seq)
+print(num_list)
+
+another_num_seq = range(0, 20, 5)
+another_num_list = list(another_num_seq)
+print(another_num_list)
+
+# list in list
+list_in_list = [
+        ["my first list", "quinn", "books"],
+        ["my second list", "another quin", "many more books"]
+    ]
+
+print(list_in_list)
+print(list_in_list[0])
+print(list_in_list[1][2])
+
+# list extension
+part_A = [1, 2, 3, 4, 5]
+part_B = [6, 7, 8, 9, 10]
+part_A.extend(part_B)
+print(part_A)
